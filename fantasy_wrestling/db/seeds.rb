59fd2401f1a07d5eafe5cc4c778c77be5a5d05db
Adam Nattrass <nattrassadam@gmail.com>
2017-02-26 17:34:02 +0000

    new seed data created

--- fantasy_wrestling/db/seeds.rb
+++ fantasy_wrestling/db/seeds.rb
@@ -12,3 +12,36 @@ Wrestler.create(
       }
 )
 
+Wrestler.create(
+      {
+        name: "Bray Wyatt", 
+        hometown: "Parts Unkown", 
+        finisher: "Sister Abigail", 
+        weight: 285, 
+        value: 85, 
+        image: "http://img03.deviantart.net/b2d1/i/2015/288/a/0/wwe_immortals___bray_wyatt__render_3__by_wyruzzah-d9d6pyp.png"
+      }
+)
+
+Wrestler.create(
+      {
+        name: "Kevin Owens", 
+        hometown: "Maryville, Quebeck, Canada", 
+        finisher: "Pop Up Powerbomb", 
+        weight: 266, 
+        value: 95, 
+        image: "http://pre12.deviantart.net/b123/th/pre/i/2016/264/a/6/kevin_owens_png_by_cmorigins-daiefuy.png"
+      }
+)
+
+Wrestler.create(
+      {
+        name: "Chris Jericho", 
+        hometown: "Winipeg, Canada", 
+        finisher: "Codebreaker", 
+        weight: 227, 
+        value: 80, 
+        image: "http://2.bp.blogspot.com/-1eU_K9G4nuY/UQdFkz1-rHI/AAAAAAAAGIo/SlB8ImouwQg/s1600/Chris+Jericho+WWE+Y2J.gif"
+      }
+)
+
