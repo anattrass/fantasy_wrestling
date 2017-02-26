aaeb64910f87b23359d7a636eee25c70549175b9
Adam Nattrass <nattrassadam@gmail.com>
2017-02-26 17:01:36 +0000

    api for showing all wrestlers and one single wrestler working

---
+++ fantasy_wrestling/app/controllers/wrestlers_controller.rb
@@ -0,0 +1,24 @@
+class WrestlersController < ApplicationController
+
+  
+  def index
+    wrestlers = Wrestler.all  
+    render :json => wrestlers.to_json()
+  end
+
+  def show
+      wrestler = Wrestler.find(params[:id])
+      render :json => artist.as_json()
+  end
+
+  def create
+    wrestler = Wrestler.create( wrestler_params )
+    render json: wrestler, status: :created
+  end
+
+  private
+  def wrestler_params
+    params.require(:wrestler).permit([:name, :hometown, :finisher, :weight, :value])
+  end
+
+end
